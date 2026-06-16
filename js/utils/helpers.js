5b3fc491cf0990191e364196e7f1f27ef22e5473
Tanuj-Rana <tanujrana581@gmail.com>
2026-06-16 19:11:13 +0530

    Update helpers.js

---
+++ js/utils/helpers.js
@@ -0,0 +1,57 @@
+export function formatDate(dateString) {
+
+    return new Date(dateString)
+        .toLocaleDateString(
+            "en-US",
+            {
+                month: "short",
+                day: "numeric"
+            }
+        );
+
+}
+
+export function truncateText(
+    text,
+    length = 120
+) {
+
+    if (!text) {
+
+        return "";
+
+    }
+
+    if (text.length <= length) {
+
+        return text;
+
+    }
+
+    return text.substring(0, length) + "...";
+
+}
+
+export function debounce(
+    func,
+    delay = 500
+) {
+
+    let timeout;
+
+    return (...args) => {
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(
+            () => {
+
+                func(...args);
+
+            },
+            delay
+        );
+
+    };
+
+}
